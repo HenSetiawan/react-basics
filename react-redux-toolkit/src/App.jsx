32c54909267ffa32b7290b7ce9a4827941176f08
hensetiawan <hendystyawan74@gmail.com>
2023-10-04 20:34:04 +0700

    create cart drawer

--- react-redux-toolkit/src/App.jsx
+++ react-redux-toolkit/src/App.jsx
@@ -2,14 +2,21 @@ import { Navbar, Container, Button, Card, Row, Col } from "react-bootstrap";
 import getAllProducts from "./services/product";
 import { useState, useEffect } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsTrash } from "react-icons/bs";
 import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [isCartShowEd, setIsCartShowEd] = useState(false);
 
   const handleGetProducts = async () => {
     const dataProducts = await getAllProducts();
     setProducts(dataProducts);
+  };
+
+  const handleShowCart = () => {
+    setIsCartShowEd((showed) => (showed = !showed));
+    document.body.classList.toggle('hidden');
   };
 
   useEffect(() => {
@@ -22,14 +29,17 @@ function App() {
           <Navbar.Brand>
             <h3 className="fw-bold">R Store</h3>
           </Navbar.Brand>
-          <Button className="ps-4 pe-4 d-flex align-items-center">
+          <Button
+            className="ps-4 pe-4 d-flex align-items-center"
+            onClick={handleShowCart}
+          >
             <AiOutlineShoppingCart />
             <span className="ms-2">Cart (0)</span>
           </Button>
         </Container>
       </Navbar>
 
-      <Container className="mt-5">
+      <Container className='mt-5'>
         <Row>
           {products.map((product) => {
             return (
@@ -59,6 +69,44 @@ function App() {
           })}
         </Row>
       </Container>
+
+      <div className={`cart-container ${isCartShowEd ? "d-blocl" : "d-none"} `}>
+        <div className="cart-layer"></div>
+        <div className="cart-content">
+          <Container className="p-4">
+            <div className="head d-flex justify-content-between">
+              <h3>Cart</h3>
+              <button
+                onClick={handleShowCart}
+                className="p-3 btn-close"
+              ></button>
+            </div>
+            <Row className="cart-item mt-4">
+              <Col lg={2}>
+                <img
+                  src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+                  alt="image product"
+                  className="img-fluid"
+                />
+              </Col>
+              <Col lg={8}>
+                <h3>Mens Casual Premium Slim Fit T-Shirts </h3>
+                <button className="btn-none pe-3 ps-3">-</button>
+                <button disabled className="btn-none pe-3 ps-3">
+                  1
+                </button>
+                <button className="btn-none pe-3 ps-3">+</button>
+              </Col>
+              <Col lg={2}>
+                <Button variant="outline-danger">
+                  <BsTrash />
+                </Button>
+                <p className="mt-4">$ 22.3</p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
     </>
   );
 }
