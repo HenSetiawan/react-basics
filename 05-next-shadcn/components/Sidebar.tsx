feab764b5b689d25f77dc639fb9840b5902dd4a0
hensetiawan <hendystyawan74@gmail.com>
2024-05-05 20:04:47 +0700

    feat : sidebar item component

--- 05-next-shadcn/components/Sidebar.tsx
+++ 05-next-shadcn/components/Sidebar.tsx
@@ -1,13 +1,68 @@
 'use client'
 import UserItem from "./UserItem"
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+import {
+  Calculator,
+  Calendar,
+  CreditCard,
+  Settings,
+  Smile,
+  User,
+} from "lucide-react";
+
 function Sidebar() {
   return (
-      <div className="flex gap-2 flex-col border-r w-[250px] min-h-screen min-w-[250px] p-2">
-          <UserItem/>
-          <div className="grow">menus</div>
-          <div>settings</div>
+    <div className="flex gap-2 flex-col border-r w-[250px] min-h-screen min-w-[250px] p-2">
+      <UserItem />
+      <div className="grow p-2">
+        <Command style={{ overflow: "visible" }}>
+          <CommandList style={{ overflow: "visible"}}>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>
+                <Calendar className="mr-2 h-4 w-4" />
+                <span>Calendar</span>
+              </CommandItem>
+              <CommandItem>
+                <Smile className="mr-2 h-4 w-4" />
+                <span>Search Emoji</span>
+              </CommandItem>
+              <CommandItem>
+                <Calculator className="mr-2 h-4 w-4" />
+                <span>Calculator</span>
+              </CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+                <CommandShortcut>⌘P</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <CreditCard className="mr-2 h-4 w-4" />
+                <span>Billing</span>
+                <CommandShortcut>⌘B</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <Settings className="mr-2 h-4 w-4" />
+                <span>Settings</span>
+                <CommandShortcut>⌘S</CommandShortcut>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </div>
+      <div>settings</div>
     </div>
-  )
+  );
 }
 
 export default Sidebar
