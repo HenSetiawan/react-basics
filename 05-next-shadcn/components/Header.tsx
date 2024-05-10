919bcc3c93bb703e767fc80e1e94992179ed294b
hensetiawan <hendystyawan74@gmail.com>
2024-05-10 15:20:29 +0700

    feat : notification icon on header component

--- 05-next-shadcn/components/Header.tsx
+++ 05-next-shadcn/components/Header.tsx
@@ -1,10 +1,17 @@
 "use client";
+import { BellIcon } from "lucide-react";
 import { CommandDemo } from "./Command";
+import { Button } from "./ui/button";
 function Header() {
   return (
-    <div className="flex p-3 border-b">
+    <div className="flex p-3 border-b items-center">
       <div className="w-1/2">
         <CommandDemo />
+      </div>
+      <div className="w-1/2 flex justify-end">
+        <Button variant="outline" className="icon">
+          <BellIcon className="w-4 h-4" />
+        </Button>
       </div>
     </div>
   );
